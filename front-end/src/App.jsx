71d78c808cc821bb7134a4fe9e6e57b2898eefc0
Mario-Mellek <mario.samy266@gmail.com>
2023-06-29 15:57:15 +0300

    Added a LandingPage and NotFound page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PlayGame from './pages/PlayGame';
 import LeaderBoard from './pages/LeaderBoard';
+import LandingPage from './pages/LandingPage';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PlayGame />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Playgame" element={<PlayGame />} />
         <Route path="/leaderboard" element={<LeaderBoard />} />
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
