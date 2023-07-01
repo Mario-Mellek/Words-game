46cde9a78e5ff05d0b59cac9901c85f7c753afbe
Mario-Mellek <mario.samy266@gmail.com>
2023-07-01 20:39:02 +0300

    added comments

--- front-end/src/utils/APIroutes.js
+++ front-end/src/utils/APIroutes.js
@@ -1,3 +1,8 @@
+// Set host URL
 const host = 'http://localhost:8080';
+
+// Set URL for getting questions
 export const getQuestions = `${host}/game`;
+
+// Set URL for posting score
 export const postScore = `${host}/game`;
